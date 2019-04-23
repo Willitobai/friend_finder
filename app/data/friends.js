6f6c8a18823110db669f63e2f61fab56f5a14239
William Baires <baireswilliame@gmail.com>
2019-04-23 13:14:33 -0700

    more updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,110 +1,29 @@
-// Array that holds data objects, a list of all friends and applicable data
-
-// Might need to change the datatype of answers to string
-module.exports = [
+var friends = [
 	{
-		name: "Shiryu",
-		photo: "http://image.blingee.com/images19/content/output/000/000/000/7b5/793228477_1156265.gif",
-		answers: [3, 1, 1, 1, 5, 4, 1, 4, 4, 5]
+		name: "Charlie",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "Ikki",
-		photo: "http://images6.fanpop.com/image/photos/40100000/Phoenix-Ikki-saint-seiya-knights-of-the-zodiac-40116831-640-800.jpg",
-		answers: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
+		name: "Sweet Dee",
+		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Shaun",
-		photo: "http://images6.fanpop.com/image/photos/40100000/Andromeda-Shun-saint-seiya-knights-of-the-zodiac-40116459-640-800.jpg",
-		answers: [4, 1, 2, 1, 5, 5, 1, 5, 5, 5]
+		name: "Dennis",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Milo",
-		photo: "http://images6.fanpop.com/image/photos/40100000/Scorpio-Milo-scorpio-milo-40135218-640-800.jpg",
-		answers: [5, 1, 4, 1, 5, 5, 1, 5, 5, 5]
+		name: "Mac",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Athena",
-		photo: "http://images6.fanpop.com/image/photos/40100000/Athena-saint-seiya-knights-of-the-zodiac-40120871-640-800.jpg",
-		answers: [5, 1, 3, 4, 5, 5, 1, 4, 3, 5]
-	},
-	{
-		name: "Hilda",
-		photo: "https://pbs.twimg.com/profile_images/906447699439964161/yZoCaNDC.jpg",
-		answers: [4, 2, 4, 4, 5, 4, 1, 5, 5, 5]
-	},
-	{
-		name: "Marin",
-		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BOGI1ZWZkODQtN2IxMy00NTNjLTk1YmYtOGM4NTk1MTI3M2FkXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
-		answers: [4, 1, 1, 1, 3, 1, 1, 5, 2, 5]
-	},
-	{
-		name: "Tokumaru",
-		photo: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s7/olenna-tyrell-512x512.jpg",
-		answers: [5, 1, 2, 5, 5, 5, 1, 3, 4, 5]
-	},
-	{
-		name: "Shaina",
-		photo: "https://assets-auto.rbl.ms/22bd2c7094b5f310574a15e21b55450781a67c19d5ed0ad23b1abdb43b3f820f",
-		answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
-	},
-	{
-		name: "Saintia",
-		photo: "https://pbs.twimg.com/profile_images/454270645879070720/82lJeUWO.jpeg",
-		answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
-	},
-	{
-		name: "Mermaid Thetis",
-		photo: "http://assets.viewers-guide.hbo.com/large5527f803e45b1.jpg",
-		answers: [5, 3, 5, 5, 5, 5, 1, 4, 4, 5]
-	},
-	{
-		name: "Omega",
-		photo: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s5/khal-drogo-512x512.jpg",
-		answers: [5, 1, 4, 1, 5, 1, 1, 4, 2, 5]
-	},
-	{
-		name: "Aiolia",
-		photo: "https://pbs.twimg.com/profile_images/626123344891375616/BYJvXTvy.jpg",
-		answers: [3, 1, 4, 3, 5, 5, 1, 4, 3, 5]
-	},
-	{
-		name: "Andromeda",
-		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BYTc4NzhjZmUtYzczNi00ZDQxLTlhOTgtMzEyZjUxNjZmZjhiXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
-		answers: [5, 1, 5, 1, 5, 1, 1, 2, 2, 1]
-	},
-	{
-		name: "Hyoga",
-		photo: "http://vignette1.wikia.nocookie.net/gameofthrones/images/9/94/Oberyn-Martell-family-tree.jpg/revision/latest?cb=20140410141001",
-		answers: [5, 4, 5, 3, 5, 3, 4, 4, 5, 5]
-	},
-	{
-		name: "Missandei",
-		photo: "https://pbs.twimg.com/profile_images/871944819354075138/dTFQQ36_.jpg",
-		answers: [3, 1, 3, 5, 2, 1, 1, 5, 5, 5]
-	},
-	{
-		name: "Brienne",
-		photo: "http://game-of-thrones.club/wp-content/uploads/2017/05/brienne-of-tarth-512x512-1.jpg",
-		answers: [3, 1, 1, 5, 5, 5, 1, 5, 5, 5]
-	},
-	{
-		name: "Zerocha",
-		photo: "https://topicimages.mrowl.com/large/chaseosburn/chaos_is_a_ladder/actors/richard_madden__robb_stark_1.jpg",
-		answers: [5, 3, 4, 3, 5, 5, 1, 5, 4, 5]
-	},
-	{
-		name: "Poseidon",
-		photo: "https://pbs.twimg.com/profile_images/748890621843271680/p-_aNCMB.jpg",
-		answers: [5, 5, 5, 1, 5, 1, 1, 3, 3, 3]
-	},
-	{
-		name: "Ades",
-		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BM2I1NTgwZDUtYzI3ZC00MzY3LWJhNmYtYWE0NmNkYjA3YWJhXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
-		answers: [4, 5, 4, 1, 5, 3, 2, 3, 4, 5]
+		name: "Frank",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
-
 ];
 
-// module.exports.push2arr = function(data) {
-// 	module.exports.allFriends.push(data);
-// }
+module.exports = friends;
