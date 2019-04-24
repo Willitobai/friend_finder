e751f51b43d72b5dfca27646f2ef16a101090527
William Baires <baireswilliame@gmail.com>
2019-04-23 23:42:03 -0700

    dead

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,36 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
-		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+		name: "Rick Grimes",
+		photo: "https://nyoobserver.files.wordpress.com/2018/11/twd_905_gp_0618_0158_rt.jpg?quality=80&w=970",
+		scores: [5, 1, 2, 3, 4, 2, 1, 4, 2, 3]
 	},
+
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
-		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+		name: "Daryl Dickson",
+		photo: "https://i.pinimg.com/originals/d8/6a/5d/d86a5d727f64dc53f829724ac8c6dca4.jpg",
+		scores: [3, 2, 4, 1, 5, 3, 2, 5, 2, 3]
 	},
+
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
-		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+		name: "Carol",
+		photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2014/11/the-walking-dead-episode-513-carol-mcbride-1200-c.jpg",
+		scores: [4, 5, 3, 4, 2, 5, 5, 1, 2, 3]
 	},
+	
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
-		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+		name: "Maggie",
+		photo: "https://i.pinimg.com/originals/22/3b/ea/223beabca05af8c16144cdbe2c275b2c.jpg",
+		scores: [1, 4, 5, 2, 1, 4, 2, 2, 5, 5]
 	},
+
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
-		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
-	}
+		name: "Glenn",
+		photo: "https://i.ytimg.com/vi/iZDOjR6QkMU/maxresdefault.jpg",
+		scores: [2, 3, 1, 5, 3, 3, 2, 5, 1, 3]
+	},
+
+	
 ];
+
 
 module.exports = friends;
