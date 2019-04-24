6c14608e82820a5f3fa77fde20cf63bb8d697dfe
William Baires <baireswilliame@gmail.com>
2019-04-24 10:00:49 -0700

    deads

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,31 +2,31 @@ var friends = [
 	{
 		name: "Rick Grimes",
 		photo: "https://nyoobserver.files.wordpress.com/2018/11/twd_905_gp_0618_0158_rt.jpg?quality=80&w=970",
-		scores: [5, 1, 2, 3, 4, 2, 1, 4, 2, 3]
+		scores: [5, 1, 2, 3, 4, 5, 1, 4, 2, 3]
 	},
 
 	{
 		name: "Daryl Dickson",
 		photo: "https://i.pinimg.com/originals/d8/6a/5d/d86a5d727f64dc53f829724ac8c6dca4.jpg",
-		scores: [3, 2, 4, 1, 5, 3, 2, 5, 2, 3]
+		scores: [3, 1, 4, 1, 5, 4, 2, 5, 2, 3]
 	},
 
 	{
 		name: "Carol",
 		photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2014/11/the-walking-dead-episode-513-carol-mcbride-1200-c.jpg",
-		scores: [4, 5, 3, 4, 2, 5, 5, 1, 2, 3]
+		scores: [4, 1, 5, 2, 4, 5, 3, 1, 2, 3]
 	},
 	
 	{
 		name: "Maggie",
 		photo: "https://i.pinimg.com/originals/22/3b/ea/223beabca05af8c16144cdbe2c275b2c.jpg",
-		scores: [1, 4, 5, 2, 1, 4, 2, 2, 5, 5]
+		scores: [1, 4, 5, 3, 2, 5, 4, 3, 1, 2]
 	},
 
 	{
 		name: "Glenn",
 		photo: "https://i.ytimg.com/vi/iZDOjR6QkMU/maxresdefault.jpg",
-		scores: [2, 3, 1, 5, 3, 3, 2, 5, 1, 3]
+		scores: [2, 3, 1, 5, 3, 4, 2, 5, 1, 4]
 	},
 
 	
